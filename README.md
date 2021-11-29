7a5db362be6d5ef17542af3303502e22108c6f9c
WeslleyQi0Dev <weslleyqi0@gmail.com>
2021-11-28 22:38:54 -0300

    Add README

---
+++ README.md
@@ -0,0 +1,6 @@
+# Digital Innovation One
+## Carrefour Android Developer
+
+### Trilha Kotlin
+
+Projeto Kotlin para o estudo de **Fundamentos de Orientação a Objetos com Kotlin**
--- src/one/digitalinnovationone/digionebank/Pessoa.kt
+++ src/one/digitalinnovationone/digionebank/Pessoa.kt
@@ -1,4 +1,13 @@
 package one.digitalinnovationone.digionebank
 
-class Pessoa {
+class Pessoa (
+    var nome : String = "Weslley",
+    var cpf : String = "088.002.000-99"
+)
+
+fun main() {
+    val weslley = Pessoa()
+
+    println(weslley.nome)
+    println(weslley.cpf)
 }
