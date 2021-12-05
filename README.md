61e267a4998c3e35f602a18a74c5bdc9bc7a81b3
WeslleyQi0Dev <weslleyqi0@gmail.com>
2021-12-05 18:28:52 -0300

    update to apply the private modifier to set function of cpf property

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ### Trilha Kotlin
 
 Projeto Kotlin para o estudo de **Fundamentos de Orientação a Objetos com Kotlin**
+
--- src/one/digitalinnovationone/digionebank/Pessoa.kt
+++ src/one/digitalinnovationone/digionebank/Pessoa.kt
@@ -1,9 +1,12 @@
 package one.digitalinnovationone.digionebank
 
-class Pessoa (
-    var nome : String = "Weslley",
-    var cpf : String = "088.002.000-99"
-)
+class Pessoa{
+    var nome: String = "Weslley"
+
+    var cpf: String = "088.002.000-99"
+    private set
+
+}
 
 fun main() {
     val weslley = Pessoa()
